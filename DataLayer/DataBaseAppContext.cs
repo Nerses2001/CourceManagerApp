9a054e8ee4fbe867ea8943a94aa6b13a5653711c
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-11-12 12:47:52 -0800

    "Data layer and Aded DataBaseAppContext and Migrashons "

---
+++ DataLayer/DataBaseAppContext.cs
@@ -0,0 +1,27 @@
+﻿using Entity;
+using Entity.EntityConfiguration;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace DataLayer
+{
+    public class DataBaseAppContext : IdentityDbContext<CustomIdentityUser>
+    {
+        public DataBaseAppContext(DbContextOptions<DataBaseAppContext> options)
+            : base(options)
+        {
+            Users = Set<CustomIdentityUser>(); 
+        }
+
+        //    public DbSet<CustomIdentityUser> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new CustomIdentityUserConfiguration());
+
+        }
+
+    }
+}
