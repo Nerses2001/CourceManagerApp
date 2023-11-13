979ede43ba2a3a8eb7319b783796aea8c62e76ec
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-11-12 22:41:03 -0800

    "Added ICustomIdentityUserRepository and CustomIdentityUserRepository Async Methods and ICustomIdentityUserService"

--- DataLayer/IRepository/ICustomIdentityUserRepository.cs
+++ DataLayer/IRepository/ICustomIdentityUserRepository.cs
@@ -6,19 +6,17 @@ namespace DataLayer.IRepository
     {
         CustomIdentityUser GetById(string id);
         Task<CustomIdentityUser> GetByIdAsync(string id);
-        
         CustomIdentityUser GetByEmail(string email);
         Task<CustomIdentityUser> GetByEmailAsync(string email);
-        
         CustomIdentityUser GetByPhoneNumber(string phoneNumber);
         Task<CustomIdentityUser> GetByPhoneNumberAsync(string phoneNumber);
-
         Task<ICollection<CustomIdentityUser>> GetByAddressAsync(string address);
         Task<ICollection<CustomIdentityUser>> GetByNameAsync(string fName);
         Task<ICollection<CustomIdentityUser>> GetByLastNameAsync(string lName);
         Task<ICollection<CustomIdentityUser>> GetByDateOfBirthAsync(DateTime dateOfBirth);
+        Task AddAsync(CustomIdentityUser user);
+        Task UpdateAsync(CustomIdentityUser user);
+        Task RemoveAsync(CustomIdentityUser user);
 
-       
-        
     }
 }
